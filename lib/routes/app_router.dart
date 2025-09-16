6ad4a0268b01c8f71352d16f0fb929f22794b411
dduranuceva <dduran@uceva.edu.co>
2025-09-16 10:52:02 -0500

    feat: Implement Flutter app architecture with routing and theming
    - Added main application entry point with MaterialApp.router for navigation.
    - Created app router using GoRouter for managing routes and parameters.
    - Introduced AppTheme class for consistent theming across the app.
    - Developed multiple screens: HomeScreen, PasoParametrosScreen, DetalleScreen, and CicloVidaScreen.
    - Implemented BaseView widget for shared layout structure with a custom drawer.
    - Added custom drawer for navigation between screens.
    - Updated pubspec.yaml to include go_router dependency.
    - Created a diagram (flutter_app_diagram.xml) to visualize app architecture and flow.

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,38 @@
+import 'package:go_router/go_router.dart';
+import 'package:registro_clases/views/ciclo_vida/ciclo_vida_screen.dart';
+import 'package:registro_clases/views/home/home_screen.dart';
+import 'package:registro_clases/views/paso_parametros/detalle_screen.dart';
+import 'package:registro_clases/views/paso_parametros/paso_parametros_screen.dart';
+
+final GoRouter appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(), // Usa HomeView
+    ),
+    // Rutas para el paso de parámetros
+    GoRoute(
+      path: '/paso_parametros',
+      builder: (context, state) => const PasoParametrosScreen(),
+    ),
+
+    // !Ruta para el detalle con parámetros
+    GoRoute(
+      path:
+          '/detalle/:parametro/:metodo', //la ruta recibe dos parametros los " : " indican que son parametros
+      builder: (context, state) {
+        //*se capturan los parametros recibidos
+        // declarando las variables parametro y metodo
+        // es final porque no se van a modificar
+        final parametro = state.pathParameters['parametro']!;
+        final metodo = state.pathParameters['metodo']!;
+        return DetalleScreen(parametro: parametro, metodoNavegacion: metodo);
+      },
+    ),
+    //!Ruta para el ciclo de vida
+    GoRoute(
+      path: '/ciclo_vida',
+      builder: (context, state) => const CicloVidaScreen(),
+    ),
+  ],
+);
